047d598d7a4d65f9ff06af0951386176d7f4df8d
szaboako <szaboako2@gmail.com>
2022-02-22 13:22:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+0. Swap our ETH to WETH
+1. Deposit some ETH into Aave
+2. Borrow some asset with ETH collateral
+3. Repay everything back
+
+everything is the same for other swaps like:
+paraswap, uniswap
+
+Testing
+
+Integrations test: Kovan
+Unit tests: Mainnet-fork
---
+++ scripts/aave_borrow.py
@@ -0,0 +1,106 @@
+from ast import And
+from brownie import network, config, interface
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+from web3 import Web3
+
+#0.1
+AMOUNT = Web3.toWei(0.1, "ether")
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+    # get_weth()
+    lending_pool = get_lending_pool()
+    # Approve sending out ERC20 tokens
+    approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address, AMOUNT, account.address, 0, {"from": account})
+    tx.wait(1)
+    print("Deposited!")
+    # Borrowing
+    borrowable_eth, total_dept = get_borrowable_data(lending_pool, account)
+    # DAI -> ETH conversion rate
+    dai_eth_price = get_asset_price(config["networks"][network.show_active()]["dai_eth_price_feed"])
+    # borrowable_eth -> borrowable_dai * 95%(for safety we can lower the percentage)
+    amount_dai_to_borrow = (1/dai_eth_price) * (borrowable_eth * 0.95)
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account}
+    )
+    borrow_tx.wait(1)
+    print("Borrowing Complete")
+    get_borrowable_data(lending_pool, account)
+    #Repaying
+    repay_all(Web3.toWei(amount_dai_to_borrow, "ether"), lending_pool, account)
+    get_borrowable_data(lending_pool, account)
+
+def repay_all(amount, lending_pool, account):
+    approve_erc20(
+        Web3.toWei(amount, "ether"),
+        lending_pool,
+        config["networks"][network.show_active()]["dai_token"],
+        account,
+    )
+    repay_tx = lending_pool.repay(
+        config["networks"][network.show_active()]["dai_token"],
+        amount,
+        1,
+        account.address,
+        {"from": account},
+    )
+    repay_tx.wait(1)
+    print("All is repaid")
+
+def get_asset_price(price_feed_address):
+    #ABI
+    #Address
+    dai_eth_price_feed = interface.IAggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_dept_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_dept_eth = Web3.fromWei(total_dept_eth, "ether")
+    print(f"You have crypto thats worth {total_collateral_eth} Ethereum deposited")
+    print(f"You can borrow crypto thats value doesn't exceed {available_borrow_eth} Ethereum")
+    print(f"Your total depth is {total_dept_eth} in Ethereum")
+    return (float(available_borrow_eth), float(total_dept_eth))
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
+
+def get_lending_pool():
+    # ABI
+    # Address
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
+    
